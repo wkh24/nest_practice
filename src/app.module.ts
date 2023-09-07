d0106c9d4747f83810a5a174bbfab5a75e335abe
왕광현 <wang-kwanghyun@wang-gwanghyeon-ui-MacBookPro.local>
2023-09-07 11:36:46 +0900

    네스트 연습

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: 'tmxlzj12',
+      database: 'nest_test',
+      entities: [User],
+      synchronize: true,
+      logging: true,
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
